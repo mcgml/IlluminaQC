5ec89aaef7ab5d499407ab20df64e2b39bef2722
mcgml <mcgml@cardiff.ac.uk>
2016-08-24 11:33:10 +0100

    updated RG, fixed pct vs decimal issue

--- 1_IlluminaQC.sh
+++ 1_IlluminaQC.sh
@@ -7,7 +7,7 @@ cd $PBS_O_WORKDIR
 #Description: Quality control for Illumina sequencing data. Not for use with other instruments.
 #Author: Matt Lyon, All Wales Medical Genetics Lab
 #Mode: BY_RUN
-#Usage: mkdir /data/results/"$seqId" && cd /data/results/"$seqId" && qsub -v seqId="$seqId",sourceDir="/data/archive/miseq/$seqId" /data/diagnositcs/pipelines/IlluminaQC/IlluminaQC-"$version"/1_IlluminaQC.sh
+#Usage: mkdir /data/results/"$seqId" && cd /data/results/"$seqId" && qsub -v seqId="$seqId",sourceDir="/data/archive/miseq/$seqId" /data/diagnostics/pipelines/IlluminaQC/IlluminaQC-"$version"/1_IlluminaQC.sh
 version="dev"
 
 #TODO highest unmatched index seq -- check for sample contamination
@@ -26,7 +26,7 @@ passedSourceDir="$sourceDir"
 #get SAV metrics & check %Q30 passed QC
 /share/apps/interop-distros/interop/build/bin/bin/imaging_table "$sourceDir" | grep -vP "#|Lane|^$" | \
 awk -F, '{ density[$1]+=$6; pf[$1]+=$10; q30[$1]+=$15; n++ } END { print "Lane\tClusterDensity\tPctPassingFilter\tPctGtQ30"; for(i in density) print i"\t"density[i]/n"\t"pf[i]/n"\t"q30[i]/n; }' | \
-tee "$seqId"_sav.txt | awk '{ if (NR > 1 && $4 < 0.8) { print "Run generated insufficient Q30 data"; exit -1 } }'
+tee "$seqId"_sav.txt | awk '{ if (NR > 1 && $4 < 80) { print "Run generated insufficient Q30 data"; exit -1 } }'
 
 #convert bcls to FASTQ
 /usr/local/bin/bcl2fastq -l WARNING -R "$sourceDir" -o .
@@ -99,7 +99,7 @@ for fastqPair in $(ls Undetermined_S0_*.fastq.gz | cut -d_ -f1-3 | sort | uniq);
     #Align reads to reference genome, retain only proper pairs, sort by coordinate and convert to BAM
     /share/apps/bwa-distros/bwa-0.7.15/bwa mem \
     -M \
-    -R '@RG\tID:'"$passedSeqId"_"$laneId"'_PhiX\tSM:PhiX\tPL:ILLUMINA\tLB:'"$passedSeqId"'_PhiX' \
+    -R '@RG\tID:'"$passedSeqId"_"$laneId"'_PhiX\tSM:PhiX\tPL:ILLUMINA\tLB:'"$passedSeqId"'_PhiX\tPU:'"$passedSeqId"_"$laneId" \
     -t 12 \
     /data/db/phix/mappers/bwa/genome.fa \
     $(echo "$read1Fastq" | sed 's/\.fastq\.gz/_trimmed\.fastq/g') $(echo "$read2Fastq" | sed 's/\.fastq\.gz/_trimmed\.fastq/g') | \
@@ -169,6 +169,11 @@ rm -r tmp
 rm *.fastq "$passedSeqId"_PhiX_*_sorted.bam "$passedSeqId"_PhiX_*_sorted.bai
 rm "$passedSeqId"_PhiX_MarkDuplicatesMetrics.txt
 rm "$passedSeqId"_PhiX_realign.intervals
+
+#log with Trello
+/share/apps/node-distros/node-v4.4.7-linux-x64/bin/node \
+/data/diagnostics/scripts/TrelloAPI.js \
+"$seqId" "Passed QC. Starting analysis"
 
 #launch analyses
 for i in $(sort workdirs.list | uniq); do
