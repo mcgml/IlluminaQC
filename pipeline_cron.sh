f4267a81f45f6ac79aba2311c8eac65401df31f7
mcgml <mcgml@cardiff.ac.uk>
2017-07-31 13:37:23 +0100

    added cron script to repo

---
+++ pipeline_cron.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -euo pipefail
+
+#Description: shell script to launch bioinformatics analysis pipelines. Run as root cron job without .sh extension
+#Author:Matt Lyon
+#Date: 28/02/17
+
+function processJobs {
+    echo "checking for jobs in $1 ..."
+
+    for path in $(find "$1" -maxdepth 2 -mindepth 2 -type f -name "RTAComplete.txt" -exec dirname '{}' \;); do
+
+        #extract run info from path
+        instrumentType=$(basename $(dirname "$path"))
+        run=$(basename "$path")
+
+        #log
+	    echo "path: $path"
+        echo "run: $run"
+        echo "instrumentType: $instrumentType"
+
+        #move run to archive
+        mv "$path" /data/archive/"$instrumentType"
+
+        #change access permissions
+        chown -R transfer /data/archive/"$instrumentType"/"$run"
+        chgrp -R transfer /data/archive/"$instrumentType"/"$run"
+        chmod -R 755 /data/archive/"$instrumentType"/"$run"
+
+        #launch IlluminaQC for demultiplexing and QC
+        ssh transfer@10.59.210.245 "mkdir /data/archive/fastq/$run && cd /data/archive/fastq/$run && qsub -v sourceDir=/data/archive/$instrumentType/$run /data/diagnostics/pipelines/IlluminaQC/IlluminaQC-1.0.4/1_IlluminaQC.sh"
+
+    done
+
+}
+
+processJobs "/data/raw/miseq"
+processJobs "/data/raw/hiseq"
