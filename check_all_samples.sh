1c463ed4cb055e9b45def8cc18da472e543d02cf
Sara Rey <sara.rey.manchester@gmail.com>
2019-05-20 16:22:51 +0100

    all tests added

--- check_all_samples.sh
+++ check_all_samples.sh
@@ -3,35 +3,44 @@ CURRENTDIRECTORY=$(pwd)
 INPUTFOLDER=${CURRENTDIRECTORY%/*}/
 
 # clear sample name list
->"$INPUTFOLDER"/samplenames.list
+>samplenames.list
 
 # obtain list of samples from sample sheet
 for line in $(sed "1,/Sample_ID/d" "$INPUTFOLDER""SampleSheet.csv" | tr -d " ")
-    do
-        # obtain sample name and patient name		
-        samplename=$(printf "$line" | cut -d, -f1 | sed 's/[^a-zA-Z0-9]+/-/g')
-		echo $samplename
-		echo $samplename >> "$INPUTFOLDER"/samplenames.list
-		#mkdir $INPUTFOLDER/$samplename
-		#touch $INPUTFOLDER/$samplename".variables"
-		#touch $INPUTFOLDER/$samplename"_1_.fastq"
-		#touch $INPUTFOLDER/$samplename"_2_.fastq"
-		#touch $INPUTFOLDER/$samplename"_3_.fastq"
-		#touch $INPUTFOLDER/$samplename"_4_.fastq"
-
+do
+    # obtain sample name and patient name		
+    samplename=$(printf "$line" | cut -d, -f1 | sed 's/[^a-zA-Z0-9]+/-/g')
+	echo $samplename >> samplenames.list
     done
+
 	
-# compare samples list to generated variables files
-if [ $(find $INPUTFOLDER -maxdepth 1 -mindepth 1 -name *".variables" | wc -l | sed 's/^[[:space:]]*//g') -eq $(sort $INPUTFOLDER/samplenames.list | uniq | wc -l | sed 's/^[[:space:]]*//g') ] 
+# compare samples list to generated variables files by count 
+if ! [ $(find . -maxdepth 1 -mindepth 1 -name '*.variables' | wc -l | sed 's/^[[:space:]]*//g') -eq $(sort samplenames.list | wc -l | sed 's/^[[:space:]]*//g') ] 
+then
+	echo "Number of variables files not as expected"
+	exit 1
+else
+	echo "Expected number of variables files created"
+fi
+	
+# compare samples list to fastqs generated
+for sn in $( cat samplenames.list )
+do
+	if ! [[ $(find . -name $sn'*fastq*') ]]
 	then
-		echo "expected number of variables files created"
+		echo "Missing fastqs for sample " $sn
+		exit 1
 	else
-		echo "number of variables files not as expected"
-		#break
+		echo "Fastqs generated for sample " $sn
 	fi
-	
-# compare samples list to fastqs
-# create file of generated fastqs
+done 
 
 
-#compare to samples list
+# compare directory number in data/results/ to list of samples from sample sheet
+if ! [[ $(ls ../data/results/ | wc -l) -eq $(sort samplenames.list | wc -l | sed 's/^[[:space:]]*//g') ]]
+then
+	echo "Number of sample directories created not as expected"
+	exit 1
+else
+	echo "Expected number of sample directories created"
+fi
